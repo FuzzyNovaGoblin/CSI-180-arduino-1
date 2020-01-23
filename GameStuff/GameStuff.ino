b5ced8a7856ba82f96a1106722d76ad4b3115fb5
Hedley <grant.hedley@CHAMPLAIN.EDU>
2020-01-23 12:13:16 -0500

    all of in class stuff ignore last 2 commits

--- GameStuff/GameStuff.ino
+++ GameStuff/GameStuff.ino
@@ -1,12 +1,17 @@
 #include <Esplora.h>
+#include <math.h>
 
-const int MAX_SLIDE = 1023;
-const int SHIELD_BLOCK_DIS = 57;
+const int MAX_SLIDE = 1024;
+const int SHIELD_BLOCK_DIS = 341;
+
+bool block;
+bool hit;
 
 
 bool usedSword;
-bool usedShild;
+bool usedShield;
 bool waitingInput;
+bool calc;
 int pSword;
 int eSword;
 int pShield;
@@ -14,6 +19,7 @@ int eShield;
 
 void setup() {
   waitingInput = false;
+  calc = false;
   pSword = 0;
   eSword = 0;
   pShield = 0; 
@@ -28,22 +34,81 @@ void loop() {
   int button4 = Esplora.readButton(SWITCH_4);
   
   if(waitingInput){
-    if(!usedShild){
+    if(!usedShield){
       if(button3 == LOW){
-        
+        pShield = Esplora.readSlider()+1;
+        usedShield = true;
       }
       if(button4 == LOW){
-        
+        pShield = (Esplora.readSlider()+1) * -1;
+        usedShield = true;
       }
     }
+    if(!usedSword){
+      if(button2 == LOW){
+        pSword = Esplora.readSlider() + 1;
+        usedSword = true;
+      }
+      if(button1 == LOW){
+        pSword = (Esplora.readSlider() + 1) * -1;
+        usedSword = true;
+      }
+    }
+
+    if(usedSword && usedShield){
+      waitingInput = false;
+      usedSword = false;
+      usedShield = false;
+      //Serial.print("Swing ");
+      //Serial.println(pSword);
+      //Serial.print("Block ");
+      //Serial.println(pShield);
+      calc = true;
+    }
+  }
+  else if(calc){
+    if(eSword * pShield > 0 ){
+     block = abs(pShield - eSword) <= SHIELD_BLOCK_DIS;
+    }
+    else{
+      block = false;
+    }
+    if(eShield * pSword > 0 ){
+     hit = abs(eShield - pSword) > SHIELD_BLOCK_DIS;
+    }
+    else{
+      block = false;
+    }
+
+    if(block){
+      Esplora.writeRGB(0,0,255);  
+    }
+    else{
+      Esplora.writeRGB(255,0,0);  
+    }
+    delay(1000);
+    if(hit){
+      Esplora.writeRGB(0, 255, 0);  
+    }
+    else{
+      Esplora.writeRGB(255,128,0);  
+    }
+    delay(1000);
+    calc = false;
+    
   }
   else{
-    eShield = random(MAX_SLIDE+1) * (bool)random(2) ? -1 : 1;
-    eSword = random(MAX_SLIDE+1) * (bool)random(2) ? -1 : 1;
+    eShield = (random(MAX_SLIDE)+1) * (bool)random(2) ? -1 : 1;
+    eSword = (random(MAX_SLIDE)+1) * (bool)random(2) ? -1 : 1;
+    
+     Serial.print("Swing ");
+     Serial.println(eSword);
+     Serial.print("Block ");
+     Serial.println(eShield);
     waitingInput = true;
   }
 
-  int val = Esplora.readSlider();
-  Serial.println(val);
-  delay(500);
+  //int val = Esplora.readSlider();
+  //Serial.println(val);
+  //delay(500);
 }
